7ebc62527e7d1ee4cc82ae5dadbabb7363a05673
Thales Lima <48839701+ThalesLJ@users.noreply.github.com>
2021-08-08 06:05:42 -0300

    Criação do App e Definições Básicas no Layout

---
+++ OrganizaTudo/OrganizaTudo/Cadastro-Usuario.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OrganizaTudo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Cadastro_Usuario : ContentPage
+    {
+        public Cadastro_Usuario()
+        {
+            InitializeComponent();
+        }
+    }
+}
